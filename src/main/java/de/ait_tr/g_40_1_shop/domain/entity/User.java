d539a117cc64bc9cd7c998fad832a394fa43e867
Vitalii-Chaplygin <chapliginvitaliy@gmail.com>
2025-07-03 00:22:47 +0200

    it is ok 4

---
+++ src/main/java/de/ait_tr/g_40_1_shop/domain/entity/User.java
@@ -0,0 +1,11 @@
+package de.ait_tr.g_40_1_shop.domain.entity;
+
+import java.util.Set;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+        private Set<Role> setRole;
+
+}
